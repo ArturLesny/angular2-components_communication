e34d5fed04615fc680e34f81674cc7eae8d45e12
Artur Leśny <toudik99@gmail.com>
2017-06-27 13:58:09 +0200

    Wprowadzono testy na weryfikację zmiany wartości w polu input i czy zostanie to wysłane do innych komponentów. Przy okazji drobne zmiany w komponencie.

--- src/app/dziecko/dziecko.component.spec.ts
+++ src/app/dziecko/dziecko.component.spec.ts
@@ -1,5 +1,6 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, fakeAsync, tick, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from "@angular/forms";
+import { By } from "@angular/platform-browser";
 
 import { DzieckoComponent } from './dziecko.component';
 
@@ -24,4 +25,33 @@ describe('DzieckoComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('sprawdza czy wartość wpowadzona w polu input jest zachowywana w zmiennej "nazwa_dziecko"', fakeAsync(() => {
+    setInputValue('#input_dziecko', 'Ktoś inny u dziecka');
+    expect(component.nazwa_dziecko).toEqual('Ktoś inny u dziecka');
+  }));
+
+  it('sprawdza czy reaguje na przycisk "Zmień" i wysyła zdarzenie o zmianie zawartości pola input', fakeAsync((): void => {
+    setInputValue('#input_dziecko', 'Ktoś inny u dziecka');
+    spyOn(component.change_nazwa, 'emit');
+    let button = findElement('#input_dziecko');
+    button.click();
+    fixture.detectChanges();
+    tick();
+    expect(component.change_nazwa.emit).toHaveBeenCalledWith({ nazwa: 'Ktoś inny u dziecka' });
+  }));
+
+  function setInputValue(selector: string, value: string) {
+    fixture.detectChanges();
+    tick();
+    let input = findElement(selector);
+    input.value = value;
+    input.dispatchEvent(new Event('input'));
+    tick();
+  }
+
+  function findElement(selector: string): any {
+    return fixture.debugElement.query(By.css(selector)).nativeElement;
+  }
+
 });
--- src/app/dziecko/dziecko.component.ts
+++ src/app/dziecko/dziecko.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, Output, OnInit, DoCheck} from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-dziecko',
